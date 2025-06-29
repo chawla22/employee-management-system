b38398a31c8bd6a3f224f481136ef63bdecc3fe5
chawla22 <chawla22@usf.edu>
2025-06-29 10:30:51 +0530

    added a Manager class with Inheritance and Overriding

--- cc_6.js
+++ cc_6.js
@@ -1,4 +1,4 @@
-//creating a base class Employee with name and Department as properties
+// Step 1: creating a base class Employee with name and Department as properties
 // anding a method to describe the employee
 class Employee {
     constructor(name, department) {
@@ -8,5 +8,21 @@ class Employee {
 
     describe() {
         return `Employee Name: ${this.name}, Department: ${this.department}`;
+    }
+}
+
+// Step 2: creating a subclass Manager that extends Employee
+// using super to inherit name and department properties
+// and adding a new property teamSize
+class Manager extends Employee {
+    constructor(name, department, teamSize) {
+        super(name, department);
+        this.teamSize = teamSize;
+    }
+
+    // overriding the describe method to include teamSize
+    // using super to call the parent class describe method     
+    describe() {
+        return `${super.describe()}, Team Size: ${this.teamSize}`;
     }
 }
